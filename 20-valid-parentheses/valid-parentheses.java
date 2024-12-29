9d535768121d4b0e7e7c639d22ed9848c47394d3
Jahy Luu <44481534+furiousstrafer@users.noreply.github.com>
2024-12-29 16:22:31 -0600

    Time: 49 ms (5.03%) | Memory: 45.1 MB (5.16%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,22 +1,15 @@
-class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-
-        for(char i : s.toCharArray()){
-            if(i == '(' || i == '{' || i == '['){
-                stack.push(i);
-            }else if(stack.empty()){
-                return false;
-            }else if(stack.peek() == '(' && i == ')'){
-                stack.pop();
-            }else if(stack.peek() == '[' && i == ']'){
-                stack.pop();
-            }else if(stack.peek() == '{' && i == '}'){
-                stack.pop();
-            } else{
-                return false;
+public class Solution {
+    public static boolean isValid(String s) {
+        while (true) {
+            if (s.contains("()")) {
+                s = s.replace("()", "");
+            } else if (s.contains("{}")) {
+                s = s.replace("{}", "");
+            } else if (s.contains("[]")) {
+                s = s.replace("[]", "");
+            } else {
+                return s.isEmpty();
             }
         }
-        return stack.empty();
     }
 }
