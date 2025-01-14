35918c557614cefa0b55641dfeea1266120e7ff1
Jahy Luu <44481534+furiousstrafer@users.noreply.github.com>
2025-01-13 23:10:23 -0600

    Time: 0 ms (100.00%) | Memory: 42.5 MB (17.06%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,29 +1,27 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int top = 0;
-        int bot = matrix.length - 1;
 
-        while (top <= bot) {
+        int top = 0, bot = matrix.length -1;
+        
+
+        while(top <= bot){
             int mid = (top + bot) / 2;
-
-            if (matrix[mid][0] < target && matrix[mid][matrix[mid].length - 1] > target) {
+            if(matrix[mid][0] < target && matrix[mid][matrix[mid].length - 1] > target){
                 break;
-            } else if (matrix[mid][0] > target) {
-                bot = mid - 1;
-            } else {
-                top = mid + 1;
+            }else if(matrix[mid][0] > target){
+                bot = mid -1;
+            }else{
+                top = mid +1;
             }
         }
 
         int row = (top + bot) / 2;
 
-        int left = 0;
-        int right = matrix[row].length - 1;
+        int left =0, right = matrix[row].length-1;
 
-        while (left <= right) {
+        while(left <= right){
             int mid = (left + right) / 2;
-
-            if (matrix[row][mid] == target) {
+            if(matrix[row][mid] == target){
                 return true;
             } else if (matrix[row][mid] > target) {
                 right = mid - 1;
@@ -31,7 +29,6 @@ class Solution {
                 left = mid + 1;
             }
         }
-
-        return false;        
+        return false;
     }
 }
