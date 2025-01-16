c66c4c6aa06cedb5cc1f34ae879a143e0f0fe12e
Jahy Luu <44481534+furiousstrafer@users.noreply.github.com>
2025-01-16 01:55:40 -0600

    Time: 27 ms (23.87%) | Memory: 45.4 MB (23.04%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int max = 0;
+        for(int i:piles)
+            if(i > max) max = i;
+        
+        int left = 0, right = max;
+        int mid = 0;
+        while(left <= right){
+            long ans = 0;
+            mid = (left + right) / 2;
+            for(int i : piles){
+                ans += Math.ceil((double)i / mid);
+            }
+            if(ans <= h){
+                right = mid -1;
+            }else{
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+}
